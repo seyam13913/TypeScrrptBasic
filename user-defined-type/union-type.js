93d886d382f73eff34b33ddf831ab1abb3950d61
seyam13913 <smmatihassen@gmail.com>
2023-11-28 00:41:17 +0600

    built in type void function & uniontype learning

---
+++ user-defined-type/union-type.js
@@ -0,0 +1,8 @@
+var user;
+user = "Jubaeair Hossen";
+user = 884949;
+user = true;
+function person(name, id, pass) {
+    console.log("Hello! My name is ".concat(name, ". My registration id ").concat(id, ". I'm GpA:").concat(pass, " pass."));
+}
+person("Seyam", 737329329, 4.5);
---
+++ user-defined-type/union-type.ts
@@ -0,0 +1,13 @@
+let user: string | number | boolean;
+
+user = "Jubaeair Hossen";
+user = 884949;
+user = true;
+
+function person(name: string, id: number, pass: number) {
+  console.log(
+    `Hello! My name is ${name}. My registration id ${id}. I'm GpA:${pass} pass.`
+  );
+}
+
+person("Seyam", 737329329, 4.5);
