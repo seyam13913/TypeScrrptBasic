06c4a727f5f7aa6b5af156221d1604e867dae797
seyam13913 <smmatihassen@gmail.com>
2023-11-27 23:33:25 +0600

    first commit

---
+++ program/index.js
@@ -0,0 +1,4 @@
+var sumNumber = function (num1, num2) {
+    console.log(num1 + num2);
+};
+sumNumber(20, 20);
---
+++ program/index.ts
@@ -0,0 +1,5 @@
+const sumNumber = (num1: number, num2: number) => {
+  console.log(num1 + num2);
+};
+
+sumNumber(20, 20);
